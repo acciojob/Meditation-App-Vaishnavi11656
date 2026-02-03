ed2c1f56c6aa3900e15c60aa4a1e39af53b132a6
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2026-02-04 00:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,68 +1,61 @@
-//your JS code here. If required.
 const song = document.querySelector(".song");
-const playBtn = document.querySeleector(".play");
-const timeDisplay = document.querySelector(".time-display");
+const play = document.querySelector(".play");
 const video = document.querySelector(".video");
+const timeDisplay = document.querySelector(".time-display");
 const soundPicker = document.querySelectorAll(".sound-picker button");
-const timeButton = document.querySelectorAll(".time-select button");
+const timeButtons = document.querySelectorAll(".time-select button");
 
-let fakeduration = 600;
+let fakeDuration = 600;
+let interval;
 
-playBtn.addEventListener("click" ()=>{
-	if(song.pause){
-		song.play();
-		video.play();
-		playBtn.pause = "pause.svg";
-	}else{
-		song.pause();
-		video.pause();
-		playBtn.play = "play.svg";
-	}
+// PLAY / PAUSE
+play.addEventListener("click", function () {
+  if (song.paused) {
+    song.play();
+    video.play();
+    startTimer();
+  } else {
+    song.pause();
+    video.pause();
+    clearInterval(interval);
+  }
 });
 
-soundPicker.forEach(button =>{
-	button.addEventListener("click", function(){
-		song.src = this.getAttribute("data-sound");
-		video.src = this.getAttribute("data-video");
-		if(!song.pause){
-			song.play();
-			video.play();
-		}
-	});
+// SWITCH SOUND + VIDEO
+soundPicker.forEach(button => {
+  button.addEventListener("click", function () {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    if (!song.paused) {
+      song.play();
+      video.play();
+    }
+  });
 });
 
-timeButton.forEach(button =>{
-	button.addEventListener("click",function () {
-		fakeduration = this.getAttribute("data-time");
-		timeDisplay.textContent = formatTime(fakeDuration);
-
-	});
+// TIME BUTTONS
+timeButtons.forEach(button => {
+  button.addEventListener("click", function () {
+    fakeDuration = this.getAttribute("data-time");
+    updateDisplay(fakeDuration);
+  });
 });
 
-song.ontimeUpdate = function(){
-	let currenttime = song.currenttime;
-	let elasped = fakeduration - currenttime;
-	let seconds = Math.floor(elasped % 60);
-	let minutes - Math.floor(elasped /60);
-
-	if(seconds < 10) seconds = "0" + seconds ;
-	  timeDisplay.textContent = `${minutes}:${seconds}`;
-
-	if(currenttime >= fakeduration){
-		song.pause();
-		song.currenttime = 0;
-		video.pause();
-		playBtn.src = "play.svg";
-	}
-	
-};
-
-function formattime(time){
-	let seconds = Math.floor(time%60);
-	let minutes = Math.floor(time/60);
-	if(seconds < 10) seconds = "0" + seconds;
-	return `${minutes}:${seconds}`;
+function startTimer() {
+  clearInterval(interval);
+  interval = setInterval(function () {
+    fakeDuration--;
+    updateDisplay(fakeDuration);
+    if (fakeDuration <= 0) {
+      clearInterval(interval);
+      song.pause();
+      video.pause();
+    }
+  }, 1000);
 }
 
-
-
+function updateDisplay(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  timeDisplay.textContent = minutes + ":" + seconds;
+}
